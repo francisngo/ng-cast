325753ac8bb2a38f70db3ebdfd07e040f3668d9f
francisngo <francis.t.ngo@gmail.com>
2017-07-21 17:27:24 -0700

    add exampleVideoData and render a video to videoPlayer

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,8 @@
 angular.module('video-player')
-
-.component('app', {
-  // TODO
-});
+  .component('app', {
+    controller: function() {
+      this.videos = window.exampleVideoData;
+      this.video = window.exampleVideoData[0];
+    },
+    templateUrl: 'src/templates/app.html'
+  });
